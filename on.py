9bac49beb345f16ce04893e27741727b5949d12d
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-10-08 05:49:24 -0400

    Update on.py

--- on.py
+++ on.py
@@ -12,24 +12,20 @@ sleep(2)
 
 check = nl.check_connection()
 
-brightness = nl.get_brightness()
-
-cFx = nl.get_current_effect()
-
-cHue = nl.get_hue()
-
-cIds = nl.get_ids()
+#brightness = nl.get_brightness()
+#cFx = nl.get_current_effect()
+#cHue = nl.get_hue()
+#cIds = nl.get_ids()
 
 #cInfo = nl.get_info()
 
 #cLayout = nl.get_layout()
 
-cName = nl.get_name()
-
 #cSatu = nl.get_saturation()
 
 #cId = nl.identify()
 
+cName = nl.get_name()
 currentInfo = [powerResp, check, cFx, cHue, cIds, cName]
 
 for theInfo in currentInfo:
